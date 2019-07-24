e869dfb55911452751fd4a794b4d157303833701
weishiyao <inwsy@hotmail.com>
2019-07-24 21:06:13 +0800

    init

--- ch2_3/ch2_3_eureka_server/src/main/java/com/springcloud/book/ch2_3_eureka_server_security/config/WebSecurityConfig.java
+++ ch2_3/ch2_3_eureka_server/src/main/java/com/springcloud/book/ch2_3_eureka_server_security/config/WebSecurityConfig.java
@@ -1,14 +1,8 @@
 package com.springcloud.book.ch2_3_eureka_server_security.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * @Date: 2019/7/23 14:39
@@ -18,19 +12,9 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private static Logger log = LoggerFactory.getLogger(WebSecurityConfig.class);
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
         http.csrf().disable().httpBasic();
     }
-
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        User.UserBuilder builder = User.withDefaultPasswordEncoder();
-//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-//        manager.createUser(builder.username("test").password("123456").roles("USER").build());
-//        return manager;
-//    }
 }
