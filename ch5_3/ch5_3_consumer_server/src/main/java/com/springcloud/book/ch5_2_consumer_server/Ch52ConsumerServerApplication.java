ab7fd245ecf026993311a5c1a1a9372a434b3be8
weishiyao <inwsy@hotmail.com>
2019-07-31 19:57:19 +0800

    init

--- ch5_3/ch5_3_consumer_server/src/main/java/com/springcloud/book/ch5_2_consumer_server/Ch52ConsumerServerApplication.java
+++ ch5_3/ch5_3_consumer_server/src/main/java/com/springcloud/book/ch5_2_consumer_server/Ch52ConsumerServerApplication.java
@@ -2,12 +2,16 @@ package com.springcloud.book.ch5_2_consumer_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class Ch52ConsumerServerApplication {
 
     public static void main(String[] args) {
