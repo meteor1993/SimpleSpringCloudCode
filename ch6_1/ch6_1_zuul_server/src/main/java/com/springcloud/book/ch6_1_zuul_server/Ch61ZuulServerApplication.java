d44754adfcaf1ab64fcff28b37235881b6d052a9
inwsy <inwsy@hotmail.com>
2019-08-05 11:20:30 +0800

    init

--- ch6_1/ch6_1_zuul_server/src/main/java/com/springcloud/book/ch6_1_zuul_server/Ch61ZuulServerApplication.java
+++ ch6_1/ch6_1_zuul_server/src/main/java/com/springcloud/book/ch6_1_zuul_server/Ch61ZuulServerApplication.java
@@ -3,11 +3,11 @@ package com.springcloud.book.ch6_1_zuul_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableZuulServer
+@EnableZuulProxy
 public class Ch61ZuulServerApplication {
 
     public static void main(String[] args) {
