da21d4d9a4d25d31a3888c9e7db9709e19ae774b
dxy3377 <daixueyun0205@163.com>
2019-08-14 19:56:23 +0800

    init

---
+++ ch9_4/ch9_4_gateway_predicate_header/src/main/java/com/springcloud/book/ch9_4_gateway_predicate_header/Ch94GatewayPredicateHeaderApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.book.ch9_4_gateway_predicate_header;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch94GatewayPredicateHeaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch94GatewayPredicateHeaderApplication.class, args);
+	}
+
+}
---
+++ ch9_4/ch9_4_gateway_predicate_method/src/test/java/com/springcloud/book/ch9_4_gateway_predicate_method/Ch94GatewayPredicateMethodApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springcloud.book.ch9_4_gateway_predicate_method;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ch94GatewayPredicateMethodApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
