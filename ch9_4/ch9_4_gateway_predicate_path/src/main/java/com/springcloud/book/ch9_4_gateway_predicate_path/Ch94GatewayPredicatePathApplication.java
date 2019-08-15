fc65bfe7fd7111c0c61206e0dba890785b7ac286
dxy3377 <daixueyun0205@163.com>
2019-08-15 17:28:11 +0800

    init

---
+++ ch9_4/ch9_4_gateway_predicate_path/src/main/java/com/springcloud/book/ch9_4_gateway_predicate_path/Ch94GatewayPredicatePathApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.book.ch9_4_gateway_predicate_path;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch94GatewayPredicatePathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch94GatewayPredicatePathApplication.class, args);
+	}
+
+}
