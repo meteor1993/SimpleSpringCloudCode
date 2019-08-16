2fd4aafc343a4d8979946e45292687056b858a25
dxy3377 <daixueyun0205@163.com>
2019-08-16 17:11:16 +0800

    init

---
+++ ch9_5/ch9_5_gateway_filter_addrequestheader/src/main/java/com/springcloud/book/ch9_5_gateway_filter_addrequestheader/Ch95GatewayFilterAddrequestheaderApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.book.ch9_5_gateway_filter_addrequestheader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch95GatewayFilterAddrequestheaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch95GatewayFilterAddrequestheaderApplication.class, args);
+	}
+
+}
---
+++ ch9_5/ch9_5_gateway_filter_service/src/main/java/com/springcloud/book/ch9_5_gateway_filter_service/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.springcloud.book.ch9_5_gateway_filter_service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by daixueyun on 2019/8/16 0016.
+ */
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping("/addRequestHeader")
+    public String addRequestHeader(HttpServletRequest request){
+        String header = request.getHeader("X-Request-hello");
+        return "addRequestHeader-------------------------header:"+header;
+    }
+
+}
