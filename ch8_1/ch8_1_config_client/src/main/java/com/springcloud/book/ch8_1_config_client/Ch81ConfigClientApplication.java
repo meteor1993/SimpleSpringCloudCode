fad4196d56d91c9790ea3fb7edb64e824917c05d
weishiyao <inwsy@hotmail.com>
2019-08-17 23:45:52 +0800

    init

---
+++ ch8_1/ch8_1_config_client/src/main/java/com/springcloud/book/ch8_1_config_client/Ch81ConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.book.ch8_1_config_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch81ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch81ConfigClientApplication.class, args);
+    }
+
+}
