b35dac5e7e8a532362bfad6c1c276d80e1ff458f
weishiyao <inwsy@hotmail.com>
2019-08-18 11:54:10 +0800

    init

---
+++ ch8_1/ch8_1_config_client_refresh/src/main/java/com/springcloud/book/ch8_1_config_client_refresh/Ch81ConfigClientRefreshApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.book.ch8_1_config_client_refresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch81ConfigClientRefreshApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch81ConfigClientRefreshApplication.class, args);
+    }
+
+}
