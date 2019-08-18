5e5bafb530495c89f784deab59a3896a354d2057
weishiyao <inwsy@hotmail.com>
2019-08-18 22:31:26 +0800

    init

--- ch8_2/ch8_2_config_client/src/main/java/com/springcloud/book/ch8_2_config_client/controller/HelloController.java
+++ ch8_2/ch8_2_config_client/src/main/java/com/springcloud/book/ch8_2_config_client/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.springcloud.book.ch8_2_config_client.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Description:
  */
 @RestController
+@RefreshScope // 使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中。
 public class HelloController {
 
     @Value("${springcloud.config.dev}")
