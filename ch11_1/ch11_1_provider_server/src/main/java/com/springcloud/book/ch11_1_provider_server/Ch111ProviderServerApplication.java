4f0ef2281f2a66878c5cc349c2ee675c7dec79d3
inwsy <inwsy@hotmail.com>
2019-08-26 17:07:59 +0800

    init

---
+++ ch11_1/ch11_1_provider_server/src/main/java/com/springcloud/book/ch11_1_provider_server/Ch111ProviderServerApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.book.ch11_1_provider_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch111ProviderServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch111ProviderServerApplication.class, args);
+    }
+
+}
