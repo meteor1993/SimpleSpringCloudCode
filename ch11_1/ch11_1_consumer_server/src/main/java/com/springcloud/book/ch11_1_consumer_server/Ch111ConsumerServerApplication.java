06565f6d143b3773a7528a9376d95e977f65a2fa
inwsy <inwsy@hotmail.com>
2019-08-26 23:05:52 +0800

    init

--- ch11_1/ch11_1_consumer_server/src/main/java/com/springcloud/book/ch11_1_consumer_server/Ch111ConsumerServerApplication.java
+++ ch11_1/ch11_1_consumer_server/src/main/java/com/springcloud/book/ch11_1_consumer_server/Ch111ConsumerServerApplication.java
@@ -1,15 +1,17 @@
-package com.springcloud.book.ch11_1_provider_server;
+package com.springcloud.book.ch11_1_consumer_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 @EnableDiscoveryClient
-public class Ch111ProviderServerApplication {
+public class Ch111ConsumerServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Ch111ProviderServerApplication.class, args);
+        SpringApplication.run(Ch111ConsumerServerApplication.class, args);
     }
 
 }
