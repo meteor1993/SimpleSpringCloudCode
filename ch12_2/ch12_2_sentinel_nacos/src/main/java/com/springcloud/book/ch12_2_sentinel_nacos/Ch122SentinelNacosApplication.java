8f6d5b35f7c21ba94c1c28263bb31251f3ff47d8
inwsy <inwsy@hotmail.com>
2019-09-01 23:16:49 +0800

    init

---
+++ ch12_2/ch12_2_sentinel_nacos/src/main/java/com/springcloud/book/ch12_2_sentinel_nacos/Ch122SentinelNacosApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.book.ch12_2_sentinel_nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch122SentinelNacosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch122SentinelNacosApplication.class, args);
+    }
+
+}
---
+++ ch12_2/ch12_2_sentinel_nacos/src/test/java/com/springcloud/book/ch12_2_sentinel_nacos/Ch122SentinelNacosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springcloud.book.ch12_2_sentinel_nacos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ch122SentinelNacosApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
