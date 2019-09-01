becc0d828d00fe4e98b89b72aec7c42606d72f40
inwsy <inwsy@hotmail.com>
2019-09-01 20:21:06 +0800

    init

---
+++ ch12_2/ch12_2_zuul_server/src/test/java/com/springcloud/book/ch12_2_zuul_server/Ch122ZuulServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springcloud.book.ch12_2_zuul_server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ch122ZuulServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
