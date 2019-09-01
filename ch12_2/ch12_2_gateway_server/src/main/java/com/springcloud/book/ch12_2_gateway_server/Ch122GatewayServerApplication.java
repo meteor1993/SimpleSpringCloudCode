5549e506d1b74b631e55c664d37cbb5fb999cf97
inwsy <inwsy@hotmail.com>
2019-09-01 22:06:36 +0800

    init

---
+++ ch12_2/ch12_2_gateway_server/src/main/java/com/springcloud/book/ch12_2_gateway_server/Ch122GatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.book.ch12_2_gateway_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch122GatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch122GatewayServerApplication.class, args);
+    }
+
+}
