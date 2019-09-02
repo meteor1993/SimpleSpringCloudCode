0891404b94734328b37599b890f0e961376ce2e5
inwsy <inwsy@hotmail.com>
2019-09-02 17:33:50 +0800

    init

---
+++ ch14_1/ch14_1_dubbo_api/src/main/java/com/springcloud/book/ch14_1_dubbo_api/Ch141DubboApiApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.book.ch14_1_dubbo_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch141DubboApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch141DubboApiApplication.class, args);
+    }
+
+}
---
+++ ch14_1/ch14_1_dubbo_api/src/main/java/com/springcloud/book/ch14_1_dubbo_api/model/UserModel.java
@@ -0,0 +1,9 @@
+package com.springcloud.book.ch14_1_dubbo_api.model;
+
+/**
+ * @Date: 2019/9/2 17:32
+ * @Version: 1.0
+ * @Desc:
+ */
+public class UserModel {
+}
