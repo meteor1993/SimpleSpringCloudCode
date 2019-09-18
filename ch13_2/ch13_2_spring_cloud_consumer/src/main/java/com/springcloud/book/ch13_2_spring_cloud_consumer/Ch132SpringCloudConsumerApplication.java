3e66de51db54f2719279eeb15f3d74f647411b19
inwsy <inwsy@hotmail.com>
2019-09-18 22:43:49 +0800

    update 13.2

--- ch13_2/ch13_2_spring_cloud_consumer/src/main/java/com/springcloud/book/ch13_2_spring_cloud_consumer/Ch132SpringCloudConsumerApplication.java
+++ ch13_2/ch13_2_spring_cloud_consumer/src/main/java/com/springcloud/book/ch13_2_spring_cloud_consumer/Ch132SpringCloudConsumerApplication.java
@@ -2,12 +2,25 @@ package com.springcloud.book.ch13_2_spring_cloud_consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
 public class Ch132SpringCloudConsumerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Ch132SpringCloudConsumerApplication.class, args);
+    }
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
